f9b183d751f0038d07b60e3502c9c5ad7b0ae685
RousseauMalco <rousseaumalco@gmail.com>
2024-09-30 23:03:49 -0500

    Up to 07.

--- java-type-checker/java_type_checker/expressions.py
+++ java-type-checker/java_type_checker/expressions.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .types import JavaBuiltInTypes, JavaTypeError
+from .types import JavaBuiltInTypes, JavaTypeError, NoSuchJavaMethod
 
 
 class JavaExpression(object):
@@ -82,10 +82,11 @@ class JavaAssignment(JavaExpression):
         return self.lhs.static_type()
     
     def check_types(self):
-        if (self.lhs.static_type() != self.rhs.static_type()):
+        self.rhs.check_types()
+        if (not self.lhs.static_type().is_supertype_of(self.rhs.static_type()) and self.lhs.static_type() != self.rhs.static_type()):
             raise JavaTypeMismatchError("Cannot assign {0} to variable {1} of type {2}".format(
                 self.rhs.static_type().name, self.lhs.name, self.lhs.static_type().name))
-
+        
 
 class JavaMethodCall(JavaExpression):
     """A Java method invocation.
@@ -107,9 +108,38 @@ class JavaMethodCall(JavaExpression):
         self.receiver = receiver
         self.method_name = method_name
         self.args = args
+        self.check_types()
+        # test = self.args
+        # if (isinstance(self.args, JavaMethodCall)):
+        #     self.args[0].check_types()
 
     def static_type(self):
         return self.receiver.declared_type.method_named(self.method_name).return_type
+    
+    def check_types(self):
+        self.receiver.static_type().method_named(self.method_name)
+
+        expected_args = self.receiver.static_type().method_named(self.method_name).parameter_types
+        given_args = [element.static_type() for element in self.args]
+
+        if (len(expected_args) != len(self.args)):
+            raise JavaArgumentCountError("Wrong number of arguments for {0}.{1}(): expected {2}, got {3}".format(
+                self.receiver.static_type().name, self.method_name, len(expected_args), len(given_args)))
+        
+        error = False
+        arg_iter = iter(given_args)
+        for arg in expected_args:
+            if (not next(arg_iter).is_subtype_of(arg)):
+                error = True
+        
+        if (error):
+            expected_as_list = [element.name for element in expected_args]
+            given_as_list = [element.name for element in given_args]
+            delim = ", "
+            raise JavaTypeMismatchError("{0}.{1}() expects arguments of type ({2}), but got ({3})".format(
+                self.receiver.static_type().name, self.method_name, delim.join(expected_as_list), delim.join(given_as_list)))
+        
+        
 
 class JavaConstructorCall(JavaExpression):
     """
